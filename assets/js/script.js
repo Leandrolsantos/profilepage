9bd3f8cbc67c0babd2330233761086dc73977370
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-12-09 18:11:14 -0100

    329º dia de CTD - HTML + CSS + JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
-window.alert('Bem vindo ao meu Portifólio, clique em ok para continuar')
-window.prompt('Qual o seu nome ?')
+ // Função para capturar a informação
+ function capturarInformacao() {
+    // Captura a informação usando o prompt
+    var informacaoDigitada = prompt("Olá, qual o seu nome ?");
+
+    // Verifica se a informação não é nula ou vazia
+    if (informacaoDigitada !== null && informacaoDigitada !== "") {
+      // Exibe a informação na página HTML
+      document.getElementById("resultado").innerText = " " + informacaoDigitada;
+    } else {
+      alert("Você não digitou nenhuma informação.");
+    }
+  }
+
+  // Chama a função assim que a página é carregada
+  window.onload = capturarInformacao;
